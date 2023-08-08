742fe2ca57239820539c6f551440a82e5e803341
Yordan Yordanov <nothingstar142@gmail.com>
2023-08-08 23:00:13 +0300

    footer added styling

---
+++ doctorsgonovski/resources/views/components/footer.blade.php
@@ -0,0 +1,47 @@
+<footer class="page-footer pt-4">
+    <div class="container-fluid">
+        <div class="row px-md-3 g-3">
+            <div class="col-sm-6 col-lg-3 py-3">
+                <h5>Company</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-decoration-none">About Us</a></li>
+                    <li><a href="#" class="text-decoration-none">Career</a></li>
+                    <li><a href="#" class="text-decoration-none">Editorial Team</a></li>
+                    <li><a href="#" class="text-decoration-none">Protection</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-lg-3 py-3">
+                <h5>More</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-decoration-none">Terms & Condition</a></li>
+                    <li><a href="#" class="text-decoration-none">Privacy</a></li>
+                    <li><a href="#" class="text-decoration-none">Advertise</a></li>
+                    <li><a href="#" class="text-decoration-none">Join as Doctors</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-lg-3 py-3">
+                <h5>Our partner</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-decoration-none">One-Fitness</a></li>
+                    <li><a href="#" class="text-decoration-none">One-Drugs</a></li>
+                    <li><a href="#" class="text-decoration-none">One-Live</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-lg-3 py-3">
+                <h5>Contact</h5>
+                <p class="mb-2">351 Willow Street Franklin, MA 02038</p>
+                <a href="#" class="text-decoration-none d-block mb-2">701-573-7582</a>
+                <a href="#" class="text-decoration-none d-block mb-3">healthcare@temporary.net</a>
+
+                <h5>Social Media</h5>
+                <div class="mt-3">
+                    <a href="#" target="_blank" class="text-decoration-none me-2"><span class="mai-logo-facebook-f"></span></a>
+                    <a href="#" target="_blank" class="text-decoration-none me-2"><span class="mai-logo-twitter"></span></a>
+                    <a href="#" target="_blank" class="text-decoration-none me-2"><span class="mai-logo-google-plus-g"></span></a>
+                    <a href="#" target="_blank" class="text-decoration-none me-2"><span class="mai-logo-instagram"></span></a>
+                    <a href="#" target="_blank" class="text-decoration-none"><span class="mai-logo-linkedin"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
