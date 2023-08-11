998527a8552bbf063abf5ac9a7fcea5ee73865cb
YordanYordanov1948 <yordan_yordanov001@abv.bg>
2023-08-11 21:45:38 +0300

    meta tags styling

--- doctorsgonovski/resources/views/components/footer.blade.php
+++ doctorsgonovski/resources/views/components/footer.blade.php
@@ -1,36 +1,29 @@
 <footer class="page-footer pt-4">
     <div class="container-fluid">
         <div class="row px-md-3 g-3">
-            <div class="col-sm-6 col-lg-3 py-3">
-                <h5>Компания</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">За нас</a></li>
-                </ul>
+
+            <!-- Empty Column for Spacing -->
+            <div class="col-sm-3 col-lg-3 py-3">
+                <!-- Nothing here -->
             </div>
-            <div class="col-sm-6 col-lg-3 py-3">
-                <h5>Още</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Terms & Condition</a></li>
-                    <li><a href="#" class="text-decoration-none">Privacy</a></li>
-                    <li><a href="#" class="text-decoration-none">Advertise</a></li>
-                    <li><a href="#" class="text-decoration-none">Join as Doctors</a></li>
-                </ul>
-            </div>
-            <div class="col-sm-6 col-lg-3 py-3">
-                <h5>Нашите партньори</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">One-Fitness</a></li>
-                    <li><a href="#" class="text-decoration-none">One-Drugs</a></li>
-                    <li><a href="#" class="text-decoration-none">One-Live</a></li>
-                </ul>
-            </div>
-            <div class="col-sm-6 col-lg-3 py-3">
+
+            <!-- Контакти Section -->
+            <div class="col-sm-3 col-lg-3 py-3">
                 <h5>Контакти</h5>
-                <p class="mb-2">351 Willow Street Franklin, MA 02038</p>
+                <p>351 Willow Street Franklin, MA 02038</p>
                 <a href="#" class="text-decoration-none d-block mb-2">701-573-7582</a>
                 <a href="#" class="text-decoration-none d-block mb-3">healthcare@temporary.net</a>
+            </div>
 
-                <h5>Социални медии</h5>
+
+            <!-- Empty Column for Spacing -->
+            <div class="col-sm-3 col-lg-3 py-3">
+                <!-- Nothing here -->
+            </div>
+
+               <!-- Социални мрежи Section -->
+               <div class="col-sm-3 col-lg-3 py-3 footer-icons">
+                <h5>Социални мрежи</h5>
                 <div class="mt-3">
                     <a href="#" target="_blank" class="text-decoration-none me-2">
                         <i class="fa-brands fa-facebook"></i>
@@ -40,6 +33,7 @@
                     </a>
                 </div>
             </div>
+
         </div>
     </div>
 </footer>
--- doctorsgonovski/resources/views/layouts/main.blade.php
+++ doctorsgonovski/resources/views/layouts/main.blade.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
 
-<html>
-    <head>
-        <title>@yield('title')</title>
-        <meta charset="UTF-8">
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-        <script type="text/javascript" src="/js/app.js"></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <head>
+
+        <title>Doctor Gonovski</title>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <meta name="description" content="Открийте кабинета на Д-р Иванка Кулевска-Гоновска,
+        специалист по кардиология, и Д-р Кристиян Гоновски, ортопед.
+        Обединявайки професионализъм и грижа, предлагаме цялостна медицинска помощ,
+        адаптирана към вашите нужди.">
+
+        <meta name="keywords" content="Д-р Иванка Кулевска-Гоновска, Кардиолог,
+        Д-р Кристиян Гоновски, Ортопед, медицински услуги, кабинет, здраве, лечение,
+        специалисти, кардиология, ортопедия">
+
+        <meta name="author" content="Д-р Иванка Кулевска-Гоновска, Д-р Кристиян Гоновски">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 
     </head>
 
