ebccd670505501f07ef4ccd571029b5b1589a3e5
Rumen98 <rumenk98@gmail.com>
2023-09-13 23:03:01 +0300

    commit

--- doctorsgonovski/resources/views/about/index.blade.php
+++ doctorsgonovski/resources/views/about/index.blade.php
@@ -3,10 +3,30 @@
 @section('title', 'About Us')
 
 @section('content')
+<style>
+ .container h1 {
+            font-size: 1.5rem;
+            line-height: 1.4;
+            font-family: 'Arial, sans-serif';
+            text-align: center;
+            margin: 0 auto 20px auto;
+            max-width: 750px;
+            color: #333;
+            background-color: #f9f9f9;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+    </style>
     <div class="container mt-5 pb-5">
         <div class="text-center mb-4">
-            <h1>Your Text Here</h1>
+            <h1>Добре дошли в нашия медицински кабинет, 
+                където вашето здраве и благополучие са на първо място. 
+                С голямо удоволствие ви представяме нашите водещи специалисти, 
+                които са посветени на задълбочените медицински прегледи и индивидуален 
+                подход към всеки пациент.</h1>
         </div>
+
 
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
 
