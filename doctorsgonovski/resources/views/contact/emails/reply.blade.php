cb52a2b2b2778a5d98124ceb847953142d420ba8
YordanYordanov1948 <yordan_yordanov001@abv.bg>
2023-09-16 14:13:07 +0300

    bug fixing

--- doctorsgonovski/resources/views/contact/emails/reply.blade.php
+++ doctorsgonovski/resources/views/contact/emails/reply.blade.php
@@ -1,22 +1,23 @@
-<div style="font-family: 'Arial', sans-serif; padding: 40px; background-color: #f5f5f5; color: #333;">
+<div class="outer-container">
 
-    <div style="max-width: 700px; margin: 0 auto; background-color: #ffffff; padding: 40px 40px 60px 40px; border-radius: 12px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);">
+    <div class="inner-container">
 
-        <h2 style="border-bottom: 2px solid #ff7f00; padding-bottom: 20px; margin-bottom: 30px; color: #ff7f00; font-size: 24px;">[Your Medical Cabinet's Name] <i class="fas fa-hospital" style="color: #ff7f00;"></i></h2>
+        <h2 class="header-section"><span class="medical-cabinet-name">Doctors Gonovski</span>
+        <i class="fas fa-hospital header-icon"></i></h2>
 
-        <p style="font-size: 18px;">Здравейте, {{ $name }},</p>
+        <p class="greeting-text">Здравейте, {{ $name }},</p>
 
-        <p style="font-size: 18px;">Благодарим Ви, че се свързахте с нас. Отговорихме на Вашия запитване по-долу:</p>
+        <p class="thank-you-text">Благодарим Ви, че се свързахте с нас. Отговорихме на Вашия запитване по-долу:</p>
 
         <!-- This is where the specific reply would go -->
-        <p style="background-color: #fff3e5; padding: 20px; border-radius: 8px; font-size: 18px; border: 1px solid #ff7f00;">[Your specific reply to the inquiry]</p>
+        <p class="response-text"></p>
 
-        <p style="font-size: 18px;">Ако имате допълнителни въпроси или нужда от допълнителна информация, моля не се колебайте да се свържете отново с нас.</p>
+        <p class="additional-info-text">Ако имате допълнителни въпроси или нужда от допълнителна информация, моля не се колебайте да се свържете отново с нас.</p>
 
-        <p style="font-size: 18px; margin-bottom: 40px;">Благодарим Ви за доверието и се надяваме да Ви бъдем полезни!</p>
+        <p class="goodbye-text">Благодарим Ви за доверието и се надяваме да Ви бъдем полезни!</p>
 
-        <p style="font-size: 18px;">С уважение,</p>
-        <p style="font-size: 18px;">[Your Medical Cabinet's Name]</p>
+        <p class="signature-text">С уважение,</p>
+        <p class="signature-text">Doctors Gonovski</p>
 
     </div>
 </div>
